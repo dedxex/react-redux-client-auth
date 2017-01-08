5cee67a142a6ec750ce45f345a96dbb002c1c27c
taranjeet <taranjeetplay@gmail.com>
2017-01-08 11:50:56 +0530

    theme added

--- src/components/dashboard/feature.js
+++ src/components/dashboard/feature.js
@@ -2,6 +2,7 @@ import React,{ Component } from 'react';
 import { connect } from 'react-redux';
 import SideBar from './SideBar';
 import Stats from './Stats';
+import Table from './table';
 import * as actions from '../../actions';
 class Feature extends Component{
     componentWillMount() {
@@ -16,6 +17,8 @@ class Feature extends Component{
                         <main className="col-sm-9 offset-sm-3 col-md-10 offset-md-2 pt-3">
                             <h1>Dashboard</h1>
                             <Stats></Stats>
+                            <h2>Section title</h2>
+                            <Table></Table>
                         </main>
                     </div>
                 </div>
